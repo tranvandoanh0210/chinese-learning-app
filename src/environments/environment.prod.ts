0b1e87b41a663a1b024478c7337e1d31c886555a
Tran Van Doanh <tranvandoanh0210@gmail.com>
2025-10-21 09:29:42 +0700

    update token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     owner: 'tranvandoanh0210',
     repo: 'chinese-learning-app',
     branch: 'main',
-    token: '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',
+    token: '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',
     dataPath: 'src/util/data.ts',
   },
 };
